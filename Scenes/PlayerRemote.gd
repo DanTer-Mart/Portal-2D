069837d330623008464bcc09f3e2807128502b53
DanTer-Mart <120460211+DanTer-Mart@users.noreply.github.com>
2025-01-29 13:24:09 +0500

    еще картинки + сцену украсил + физика

--- Scenes/PlayerRemote.gd
+++ Scenes/PlayerRemote.gd
@@ -1,10 +1,7 @@
 extends CharacterBody2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
-	velocity.y += delta * 500.0
-	move_and_collide(delta * velocity)
+func _physics_process(delta):
+	velocity = Vector2.ZERO
+	# Нормализуем вектор скорости, чтобы избежать ускорения при диагональном движении
+	if velocity.length() > 0:
+		velocity = velocity.normalized()
