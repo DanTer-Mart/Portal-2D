1babc138935d7dc1f49004bf5964844e0f63953c
DanTer-Mart <120460211+DanTer-Mart@users.noreply.github.com>
2025-02-18 11:05:20 +0500

    opening

--- Scenes/logo.gd
+++ Scenes/logo.gd
@@ -3,3 +3,5 @@ extends Sprite2D
 func _ready() -> void:
 	var viewport_size = get_viewport().get_visible_rect().size
 	position = viewport_size / 2
+	position.x -= 200
+	$"../Label".position = viewport_size / 2
