a815ef1cba37e40446e1fb6746dd7634fd9aec05
DanTer-Mart <120460211+DanTer-Mart@users.noreply.github.com>
2025-02-19 13:07:18 +0500

    опенинг готов кажись

--- Scripts/logo.gd
+++ Scripts/logo.gd
@@ -2,6 +2,16 @@ extends Sprite2D
 
 func _ready() -> void:
 	var viewport_size = get_viewport().get_visible_rect().size
-	position = viewport_size / 2
-	position.x -= 200
-	$"../Label".position = viewport_size / 2
+	var array = [$"../WhoIsAuthor",$".",$"../Portal2",$"../Portal1"]
+	for i in array.size():
+		array[i].position = viewport_size / 2
+		if i > 1:
+			array[i].position.x -= 200
+			# да, я сам этот скрипт писал без ии!!
+	position.y -= 750
+	$"../Portal1".position.y += 475
+	$"../Portal2".position.y -= 475
+
+
+func _TOMenu() -> void:
+	$AnimationLogo.play("end")
